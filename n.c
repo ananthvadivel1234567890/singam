5d84704e076b3cb65c8588167555c86e18a4989e
ananthvadivel1234567890 <35652942+ananthvadivel1234567890@users.noreply.github.com>
2018-02-09 16:13:28 +0530

    Create n.c

---
+++ n.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    n=int (row_input())
+    num=n
+    sum=0
+    while(num>0);
+    rem=num%10
+    sum=sum*10+rem
+    num=num/10
+    if(sum=n=n);
+    print('polindrome')
+    else;
+    print('not polindrome')
